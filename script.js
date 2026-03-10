1721d212d3fd71117e5e271e9319c287ae954b9b
claudioclfok <claudiofernandezclf@gmail.com>
2026-03-10 16:58:36 -0300

    Add function to manage shopping cart items

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
+function agregarCarrito(id,nombre,precio){
+
+let producto={
+id:id,
+nombre:nombre,
+precio:precio
+};
+
+carrito.push(producto);
+
+localStorage.setItem("carrito",JSON.stringify(carrito));
+
+alert("Producto agregado al carrito");
+
+}
