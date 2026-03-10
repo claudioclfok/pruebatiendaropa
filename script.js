896bf9d25e8f85f9930ea27554991c1116296450
claudioclfok <claudiofernandezclf@gmail.com>
2026-03-10 19:22:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,43 @@ let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 function agregarCarrito(id,nombre,precio){
 
-let producto={
+carrito.push({
 id:id,
 nombre:nombre,
 precio:precio
-};
-
-carrito.push(producto);
+});
 
 localStorage.setItem("carrito",JSON.stringify(carrito));
 
 alert("Producto agregado al carrito");
+
+}
+
+function cargarCarrito(){
+
+let contenedor = document.getElementById("carrito");
+let total = 0;
+
+contenedor.innerHTML="";
+
+carrito.forEach(p=>{
+
+contenedor.innerHTML += `
+<div>
+${p.nombre} - $${p.precio}
+</div>
+`;
+
+total += p.precio;
+
+});
+
+document.getElementById("total").innerText = total;
+
+}
+
+function pagar(){
+
+alert("Simulación de pago.\nAquí luego se conecta Mercado Pago.");
 
 }
